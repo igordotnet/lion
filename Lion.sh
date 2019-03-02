516cd1c242bbe93901dbbae5e19526ff890cc553
Igor Lucic <iglucic@e1z2r6p4.42.us.org>
2019-03-01 23:45:15 -0800

    close

--- Lion.sh
+++ Lion.sh
@@ -3,7 +3,6 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get purge openssh-server
 yes Yes | sudo apt-get install openssh-server
-sudo service ssh status
 sudo sed -i -e 's/#Port 22/Port 4141/' /etc/ssh/sshd_config
 sudo service ssh restart
 sudo service ssh status
